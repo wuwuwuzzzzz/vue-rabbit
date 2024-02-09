a10c114d8e7c7865cc86ac89deaa04bdb9d2184b
wxz <759126349@qq.com>
2024-02-09 14:48:28 +0800

    详情页 - 适配不同 title 和数据列表

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -2,13 +2,25 @@
 import { getHotGoodsAPI } from '@/apis/detail.js';
 import { useRoute } from 'vue-router';
 
+const prop = defineProps({
+  hotType: {
+    type: Number
+  }
+})
+
+const TYPEMAP = {
+  1: '24小时热榜',
+  2: '周热榜'
+}
+
+const title = computed(() => TYPEMAP[prop.hotType])
 const hotList = ref([])
 const route = useRoute()
 
 const getHotList = async () => {
   const res = await getHotGoodsAPI({
     id: route.params.id,
-    type: 1
+    type: prop.hotType
   })
   hotList.value = res.result
 }
@@ -20,7 +32,7 @@ onMounted(() => {
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
       <img :src="item.picture" alt="" />
