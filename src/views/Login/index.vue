28b8792a56ab338b0a3c2934208af053b5828ca4
wxz <759126349@qq.com>
2024-02-09 16:28:07 +0800

    登录 - 表单校验实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,22 @@
 <script setup>
 
-</script>
+// 表单校验（账号 + 密码）
+const form = ref({
+  account: '',
+  password: ''
+})
 
+// 表单校验规则
+const rules = {
+  account: [
+    { required: true, message: '请输入账号', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码长度在 6 到 14 个字符', trigger: 'blur' }
+  ]
+}
+</script>
 
 <template>
   <div>
@@ -24,13 +39,12 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
-                     status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon hide-required-asterisk>
+              <el-form-item label="账户" prop="account">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
