b83026bf3467e77ce0f722581fbd4bad9b34052a
wxz <759126349@qq.com>
2024-02-10 01:49:06 +0800

    登录 - 请求拦截器携带 Token

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,18 +4,23 @@ import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user';
 import router from '@/router/index.js';
 
-// 创建axios实例
+// 创建 axios 实例
 const httpInstance = axios.create({
   baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
 });
 
-// axios请求拦截器
+// axios 请求拦截器
 httpInstance.interceptors.request.use(config => {
+  const userStore = useUserStore()
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
-// axios响应式拦截器
+// axios 响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
   ElMessage.error({
     message: e.response.data.msg
