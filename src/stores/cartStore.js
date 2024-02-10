98139ed0554f8d9797973420cc7bca933037cf48
wxz <759126349@qq.com>
2024-02-10 13:52:57 +0800

    购物车 - 本地 - 全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -21,11 +21,17 @@ export const useCartStore = defineStore('cart', () => {
     item.selected = selected;
   }
 
+  const allCheck = selected => {
+    cartList.value.forEach(item => item.selected = selected);
+  }
+
   const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
 
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
-  return { cartList, addCart, delCart, singleCheck, allCount, allPrice }
+  const isAll = computed(() => cartList.value.every(item => item.selected))
+
+  return { cartList, addCart, delCart, singleCheck, allCheck, allCount, allPrice, isAll }
 }, {
   persist: true
 });
